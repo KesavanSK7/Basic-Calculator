62254fb412e2452f7e259f8accd83d4dde333cdf
KESAVAN S <136875585+KesavanSK7@users.noreply.github.com>
2024-04-16 01:09:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const btns = document.querySelectorAll('.btn');
+const inputBox = document.getElementById('input-box');
+console.log(inputBox);
+btns.forEach(btn => {
+    btn.addEventListener("click", (event)=>{
+        if(btn.classList.contains('ac')){
+            inputBox.value ='';
+        }
+        else if(btn.classList.contains('del')){
+            let len = inputBox.value.length;
+            inputBox.value = inputBox.value.slice(0,len-1);
+        }
+        else if(btn.classList.contains('calculate')){
+            try{
+                let expression = inputBox.value.replace(/x/g, '*').replace(/÷/g, '/');
+                inputBox.value = eval(expression);
+            }
+            catch{
+                inputBox.value = 'Syntax Error';
+            }
+        }
+        else{
+            inputBox.value += event.target.textContent;
+        }
+    });
+});
